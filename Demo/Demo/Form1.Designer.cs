859a77045fc4090e0a969739b536da5c5332b6ed
zhangxiaolong <zhangxiaolong@jhj.com.cn>
2017-10-20 20:38:17 +0800

    2017102001

--- Demo/Demo/Form1.Designer.cs
+++ Demo/Demo/Form1.Designer.cs
@@ -29,33 +29,169 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(107, 48);
+            this.button1.Location = new System.Drawing.Point(63, 54);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UserName";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Tag = "UserName";
+            this.textBox1.Text = "张三";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Tag = "PassWord";
+            this.textBox2.Text = "123456";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(264, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(363, 196);
+            this.panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AppId";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(3, 82);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(340, 100);
+            this.panel2.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(127, 7);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(131, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Tag = "TimeZoneId";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TimeZoneId";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(130, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Tag = "AppId";
+            this.textBox3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "PassWord";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(63, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(613, 74);
+            this.dataGridView1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(745, 594);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Demo/Demo/Form1.cs
+++ Demo/Demo/Form1.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Demo
@@ -18,7 +15,71 @@ namespace Demo
 
         private void button1_Click(object sender, EventArgs e)
         {
+            List<LoginEntities> login = new List<LoginEntities>();
+            login.Add(GetGetControls<LoginEntities>(this));
+            this.dataGridView1.DataSource = login;
+        }
 
+        public static TResult GetGetControls<TResult>(Form form) where TResult : class,new()
+        {
+            TResult t = new TResult();
+            //将form内所有的控件属性拿到通用表ControlModel中
+            List<ControlModel> Mo = new List<ControlModel>();
+            foreach (Control con in form.Controls)
+            {
+                Mo.AddRange(GetControlModel(con));
+            }
+
+            //创建一个属性的列表
+            List<PropertyInfo> prlist = new List<PropertyInfo>();
+            //获取TResult的类型实例  反射的入口
+            Type type = typeof(TResult);
+            //获得TResult 的所有的Public 属性 并找出TResult属性和ControlModel的列名称相同的属性(PropertyInfo) 并加入到属性列表
+            Array.ForEach<PropertyInfo>(type.GetProperties(), p =>
+            {
+                if (Mo.Where(o => o.ControlTag == p.Name).Count() > 0)
+                    prlist.Add(p);
+            });
+
+            prlist.ForEach(p =>
+            {
+                if (Mo.Find(o => o.ControlTag == p.Name).ControlValue != "")
+                    p.SetValue(t, Mo.Find(o => o.ControlTag == p.Name).ControlValue, null);
+            });
+
+            return t;
+        }
+
+        public static List<ControlModel> GetControlModel(Control Con)
+        {
+            List<ControlModel> Cmodel = new List<ControlModel>();
+
+            switch (Con.GetType().ToString())
+            {
+                case "System.Windows.Forms.Label":
+                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
+                    break;
+
+                case "System.Windows.Forms.Button":
+                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
+                    break;
+
+                case "System.Windows.Forms.TextBox":
+                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
+                    break;
+
+                case "System.Windows.Forms.DateTimePicker":
+                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
+                    break;
+            }
+            if (Con.HasChildren)
+            {
+                for (int i = 0; i < Con.Controls.Count; i++)
+                {
+                    Cmodel.AddRange(GetControlModel(Con.Controls[i]));
+                }
+            }
+            return Cmodel;
         }
     }
 }
