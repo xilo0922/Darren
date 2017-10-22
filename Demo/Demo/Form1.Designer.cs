0d6be75b381ffd40b6fe57c78d8c4123caad6b21
zhangxiaolong <zhangxiaolong@jhj.com.cn>
2017-10-22 13:54:13 +0800

    控件转换操作

--- Demo/Demo/Form1.Designer.cs
+++ Demo/Demo/Form1.Designer.cs
@@ -29,25 +29,32 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
+            this.spinEdit1 = new DevExpress.XtraEditors.SpinEdit();
+            this.toggleSwitch1 = new DevExpress.XtraEditors.ToggleSwitch();
+            this.checkButton1 = new DevExpress.XtraEditors.CheckButton();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.dropDownButton1 = new DevExpress.XtraEditors.DropDownButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioGroup1 = new DevExpress.XtraEditors.RadioGroup();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toggleSwitch1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(63, 54);
+            this.button1.Location = new System.Drawing.Point(40, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -55,18 +62,9 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 95);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "UserName";
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(130, 95);
+            this.textBox1.Location = new System.Drawing.Point(40, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 0;
@@ -75,104 +73,166 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(130, 122);
+            this.textBox2.Location = new System.Drawing.Point(146, 45);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 21);
             this.textBox2.TabIndex = 1;
             this.textBox2.Tag = "PassWord";
             this.textBox2.Text = "123456";
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Location = new System.Drawing.Point(264, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(363, 196);
-            this.panel1.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "AppId";
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.dateTimePicker1);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Location = new System.Drawing.Point(3, 82);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(340, 100);
-            this.panel2.TabIndex = 2;
-            // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(127, 7);
+            this.dateTimePicker1.Location = new System.Drawing.Point(40, 72);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(131, 21);
             this.dateTimePicker1.TabIndex = 0;
             this.dateTimePicker1.Tag = "TimeZoneId";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 12);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "TimeZoneId";
-            // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(130, 43);
+            this.textBox3.Location = new System.Drawing.Point(252, 45);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 21);
             this.textBox3.TabIndex = 0;
             this.textBox3.Tag = "AppId";
             this.textBox3.Text = "1";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(61, 122);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 12);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "PassWord";
-            // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(63, 233);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 497);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(613, 74);
+            this.dataGridView1.Size = new System.Drawing.Size(865, 74);
             this.dataGridView1.TabIndex = 6;
+            // 
+            // checkEdit1
+            // 
+            this.checkEdit1.Location = new System.Drawing.Point(474, 69);
+            this.checkEdit1.Name = "checkEdit1";
+            this.checkEdit1.Properties.Caption = "checkEdit1";
+            this.checkEdit1.Size = new System.Drawing.Size(100, 19);
+            this.checkEdit1.TabIndex = 7;
+            this.checkEdit1.Tag = "CheckEdit";
+            // 
+            // spinEdit1
+            // 
+            this.spinEdit1.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.spinEdit1.Location = new System.Drawing.Point(641, 73);
+            this.spinEdit1.Name = "spinEdit1";
+            this.spinEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.spinEdit1.Size = new System.Drawing.Size(100, 20);
+            this.spinEdit1.TabIndex = 11;
+            // 
+            // toggleSwitch1
+            // 
+            this.toggleSwitch1.Location = new System.Drawing.Point(515, 121);
+            this.toggleSwitch1.Name = "toggleSwitch1";
+            this.toggleSwitch1.Properties.OffText = "Off";
+            this.toggleSwitch1.Properties.OnText = "On";
+            this.toggleSwitch1.Size = new System.Drawing.Size(95, 25);
+            this.toggleSwitch1.TabIndex = 12;
+            this.toggleSwitch1.Tag = "ToggleSwitch";
+            // 
+            // checkButton1
+            // 
+            this.checkButton1.Location = new System.Drawing.Point(614, 152);
+            this.checkButton1.Name = "checkButton1";
+            this.checkButton1.Size = new System.Drawing.Size(216, 23);
+            this.checkButton1.TabIndex = 14;
+            this.checkButton1.Tag = "CheckButton";
+            this.checkButton1.Text = "checkButton1";
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(784, 31);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(100, 20);
+            this.textEdit2.TabIndex = 15;
+            // 
+            // dropDownButton1
+            // 
+            this.dropDownButton1.Location = new System.Drawing.Point(536, 230);
+            this.dropDownButton1.Name = "dropDownButton1";
+            this.dropDownButton1.Size = new System.Drawing.Size(135, 23);
+            this.dropDownButton1.TabIndex = 16;
+            this.dropDownButton1.Text = "dropDownButton1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(45, 104);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(95, 16);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.Location = new System.Drawing.Point(515, 21);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(null, "发送"),
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(null, "未发送")});
+            this.radioGroup1.Size = new System.Drawing.Size(263, 30);
+            this.radioGroup1.TabIndex = 20;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(40, 129);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(745, 594);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(889, 583);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioGroup1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dropDownButton1);
+            this.Controls.Add(this.textEdit2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkButton1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.toggleSwitch1);
+            this.Controls.Add(this.spinEdit1);
+            this.Controls.Add(this.checkEdit1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toggleSwitch1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,17 +241,21 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private DevExpress.XtraEditors.CheckEdit checkEdit1;
+        private DevExpress.XtraEditors.SpinEdit spinEdit1;
+        private DevExpress.XtraEditors.ToggleSwitch toggleSwitch1;
+        private DevExpress.XtraEditors.CheckButton checkButton1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.DropDownButton dropDownButton1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private DevExpress.XtraEditors.RadioGroup radioGroup1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Demo/Demo/Form1.cs
+++ Demo/Demo/Form1.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Windows.Forms;
 
 namespace Demo
@@ -16,70 +14,18 @@ namespace Demo
         private void button1_Click(object sender, EventArgs e)
         {
             List<LoginEntities> login = new List<LoginEntities>();
-            login.Add(GetGetControls<LoginEntities>(this));
+            login.Add(ControlsUtilities.GetControls<LoginEntities>(this));
+            LoginEntities Getle = ControlsUtilities.GetControls<LoginEntities>(this);
+            Getle.UserName = "李四";
+            Getle.TimeZoneId = null;
+            Getle.ToggleSwitch = true;
+            LoginEntities Setle = ControlsUtilities.SetControls<LoginEntities>(this, Getle);
             this.dataGridView1.DataSource = login;
         }
 
-        public static TResult GetGetControls<TResult>(Form form) where TResult : class,new()
+        private void Form1_Load(object sender, EventArgs e)
         {
-            TResult t = new TResult();
-            //将form内所有的控件属性拿到通用表ControlModel中
-            List<ControlModel> Mo = new List<ControlModel>();
-            foreach (Control con in form.Controls)
-            {
-                Mo.AddRange(GetControlModel(con));
-            }
-
-            //创建一个属性的列表
-            List<PropertyInfo> prlist = new List<PropertyInfo>();
-            //获取TResult的类型实例  反射的入口
-            Type type = typeof(TResult);
-            //获得TResult 的所有的Public 属性 并找出TResult属性和ControlModel的列名称相同的属性(PropertyInfo) 并加入到属性列表
-            Array.ForEach<PropertyInfo>(type.GetProperties(), p =>
-            {
-                if (Mo.Where(o => o.ControlTag == p.Name).Count() > 0)
-                    prlist.Add(p);
-            });
-
-            prlist.ForEach(p =>
-            {
-                if (Mo.Find(o => o.ControlTag == p.Name).ControlValue != "")
-                    p.SetValue(t, Mo.Find(o => o.ControlTag == p.Name).ControlValue, null);
-            });
-
-            return t;
-        }
-
-        public static List<ControlModel> GetControlModel(Control Con)
-        {
-            List<ControlModel> Cmodel = new List<ControlModel>();
-
-            switch (Con.GetType().ToString())
-            {
-                case "System.Windows.Forms.Label":
-                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
-                    break;
-
-                case "System.Windows.Forms.Button":
-                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
-                    break;
-
-                case "System.Windows.Forms.TextBox":
-                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
-                    break;
-
-                case "System.Windows.Forms.DateTimePicker":
-                    Cmodel.Add(new ControlModel(Con.GetType().ToString(), Con.Tag == null ? "" : Con.Tag.ToString(), Con.Name, Con.Text));
-                    break;
-            }
-            if (Con.HasChildren)
-            {
-                for (int i = 0; i < Con.Controls.Count; i++)
-                {
-                    Cmodel.AddRange(GetControlModel(Con.Controls[i]));
-                }
-            }
-            return Cmodel;
+            radioGroup1.SelectedIndex = 0;
         }
     }
 }
